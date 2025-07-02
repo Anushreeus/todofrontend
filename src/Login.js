a2bd0edff8361e2810e3138cb48e4e5b4551689d
Anushreeus <166030203+Anushreeus@users.noreply.github.com>
2025-07-02 10:13:18 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const res = await fetch("http://localhost:8000/login", {
+    const res = await fetch("https://todobackend-pbac.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
